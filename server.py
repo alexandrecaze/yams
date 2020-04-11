2b1aa78e245dce3d55017805710a7b08762f1089
Alexandre Cazé <alexandre.caze.87@gmail.com>
2020-04-11 14:45:53 +0200

    alors (#1)
    Co-authored-by: Alexandre CAZE <alexandre.caze@manomano.com>

--- server.py
+++ server.py
@@ -8,7 +8,13 @@ DICE_MAPPING =	{1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six'}
 def hello_world():
     dice_state = np.random.randint(1, 7, 5)
     dice_values = [DICE_MAPPING[dice] for dice in dice_state]
-    return render_template('server.html', dice_values=dice_values)
+    player_names = ['Alex', 'Nico']
+    coups = ['1', '2', '3', '4', '5', '6', 'Bonus', 'Min', 'Max', 'Brelan', 'Carré', 'Pte suite', 'Gde suite', 'Yams']
+    scores = np.zeros((len(coups), len(player_names)))
+    print(coups)
+    print(player_names)
+    print(scores)
+    return render_template('server.html', dice_values=dice_values, player_names=player_names, coups=coups, scores=scores)
 
 if __name__ == '__main__':
   app.run()
