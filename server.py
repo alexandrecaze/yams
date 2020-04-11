5a9f3ed536c2aebdd28449ee28a52b81479eff1b
Nicometo <62020214+Nirasak@users.noreply.github.com>
2020-04-11 16:11:47 +0200

    add dataframe (#3)
    * add dataframe
    
    * fix spaces
    
    Co-authored-by: Nicolas ALLAIN <nallain@localhost.localdomain>

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 import numpy as np
 app = Flask(__name__)
 
@@ -10,11 +11,8 @@ def hello_world():
     dice_values = [DICE_MAPPING[dice] for dice in dice_state]
     player_names = ['Alex', 'Nico']
     coups = ['1', '2', '3', '4', '5', '6', 'Bonus', 'Min', 'Max', 'Brelan', 'Carré', 'Pte suite', 'Gde suite', 'Yams']
-    scores = np.zeros((len(coups), len(player_names)))
-    print(coups)
-    print(player_names)
-    print(scores)
-    return render_template('server.html', dice_values=dice_values, player_names=player_names, coups=coups, scores=scores)
+    scores = pd.DataFrame(data=np.zeros((len(coups), len(player_names)), dtype=int), columns=player_names, index=coups)
+    return render_template('server.html', dice_values=dice_values, player_names=player_names, coups=coups, scores=scores.to_html(classes=["table", "table-sm"], border=0, justify='left'))
 
 if __name__ == '__main__':
   app.run()
